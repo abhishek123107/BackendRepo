d8436a013fe9cfccb1b19d7e1a002da2b5cff1d0
abhishek123107 <at860583@gmail.com>
2026-01-18 11:22:05 +0530

    Fix Vercel handler signature

--- api/index.py
+++ api/index.py
@@ -16,5 +16,34 @@ from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 # Vercel serverless function handler
-def handler(request):
-    return application(request.environ, lambda status, headers: None)
+def handler(event, context):
+    # Convert Vercel event to WSGI environ
+    environ = {
+        'REQUEST_METHOD': event.get('httpMethod', 'GET'),
+        'PATH_INFO': event.get('path', '/'),
+        'QUERY_STRING': event.get('queryStringParameters', ''),
+        'CONTENT_TYPE': event.get('headers', {}).get('content-type', 'application/json'),
+        'CONTENT_LENGTH': str(len(event.get('body', ''))),
+        'SERVER_NAME': 'vercel.app',
+        'SERVER_PORT': '443',
+        'wsgi.url_scheme': 'https',
+        'wsgi.input': event.get('body', ''),
+        'wsgi.errors': sys.stderr,
+        'wsgi.version': (1, 0),
+        'wsgi.multithread': False,
+        'wsgi.multiprocess': False,
+        'wsgi.run_once': False,
+    }
+    
+    # Add headers to environ
+    for key, value in event.get('headers', {}).items():
+        environ[f'HTTP_{key.upper().replace("-", "_")}'] = value
+    
+    # Call Django application
+    response = application(environ, lambda status, headers: None)
+    
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': 'Django app is running'
+    }
